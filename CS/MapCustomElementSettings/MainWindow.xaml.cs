71533a22dfa243b2f02e11b8311ee140d78db412
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MapCustomElementSettings/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Xml;
+using System.Xml.Serialization;
+using DevExpress.Xpf.Map;
+
+namespace MapCustomElementSettings {
+    public partial class MainWindow : Window {
+        const string filepath = "..//..//disasters.xml";
+
+        List<Disaster> disasters;
+        public List<Disaster> Disasters { get { return disasters; } }
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            disasters = DeserializeXml(filepath);
+            this.DataContext = this;
+        }
+
+        List<Disaster> DeserializeXml(string filepath) {
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Disaster>));
+            XmlReader reader = new XmlTextReader(filepath);
+            List<Disaster> data = (List<Disaster>)serializer.Deserialize(reader);
+            reader.Close();
+
+            return data;
+        }
+    }
+
+    class MarkerTemplateSelector : DataTemplateSelector {
+        public DataTemplate AirRaidsDataTemplate { get; set; }
+        public DataTemplate BushfiresDataTemplate { get; set; }
+        public DataTemplate CycloneDataTemplate { get; set; }
+        public DataTemplate SinkingDataTemplate { get; set; }
+        public DataTemplate EpidemicDataTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container) {
+            Disaster disaster = item as Disaster;
+            if (disaster == null) return base.SelectTemplate(item, container);
+
+            MapCustomElement element = container as MapCustomElement;
+            switch (disaster.Type) {
+                case "Air Raids": return AirRaidsDataTemplate;
+                case "Bushfires": return BushfiresDataTemplate;
+                case "Cyclone": return CycloneDataTemplate;
+                case "Sinking": return SinkingDataTemplate;
+                case "Epidemic": return EpidemicDataTemplate;
+                default: return base.SelectTemplate(item, container);
+            }
+        }
+    }
+
+    public class Disaster {
+        public string Type { get; set; }
+        public double LocationLatitude { get; set; }
+        public double LocationLongitude { get; set; }
+        public string LocationName { get; set; }
+        public int Deaths { get; set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to use several templates for automatically generated map custom elements
+
+
+This example demonstrates how to use several templates for automatically generated map custom elements.<br />For this, specify the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapMapCustomElementSettings_ContentTemplateSelectortopic">ContentTemplateSelector</a> property of the <a href="https://documentation.devexpress.com/#WPF/clsDevExpressXpfMapMapCustomElementSettingstopic">MapCustomElementSettings</a> object assigned to the <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfMapListSourceDataAdapter_ItemSettingstopic">ListSourceDataAdapter.ItemSettings</a> property.
+
+<br/>
+
+
